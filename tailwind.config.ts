33383fc6ca9941352c5f77b9bf6f61da73ca695b
emmanuelsalako28 <emmanuelsalako28@gmail.com>
2026-01-30 10:06:26 +0100

    font update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -102,6 +102,10 @@ export default {
 			animation: {
 				'accordion-down': 'accordion-down 0.2s ease-out',
 				'accordion-up': 'accordion-up 0.2s ease-out'
+			},
+			fontFamily: {
+				sans: ['Helvetica', 'Arial', 'sans-serif'],
+				gotham: ['Montserrat', 'sans-serif'],
 			}
 		}
 	},
